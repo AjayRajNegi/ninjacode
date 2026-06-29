7942c8adac9b15ff52051d464a5eaebdd1bbb0ec
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-06-29 16:24:30 +0530

    themeRoot element

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -4,29 +4,40 @@ import { Header } from "./components/header";
 import { Input } from "./components/input-bar";
 import { ToastProvider } from "./providers/toast";
 import { KeyboardLayerProvider } from "./providers/keyboard-layer";
-import { DialogProvider } from "./providers/dialog";
+import { DialogProvider } from "./components/dialog";
+import { ThemeProvider, useTheme } from "./providers/theme";
+
+function ThemedRoot() {
+  const { colors } = useTheme();
+
+  return (
+    <box
+      alignItems="center"
+      justifyContent="center"
+      backgroundColor={colors.background}
+      width="100%"
+      height="100%"
+      gap={2}
+    >
+      <Header />
+      <box width="100%" maxWidth={78} paddingX={2}>
+        <Input onSubmit={() => {}} />
+      </box>
+    </box>
+  );
+}
 
 function App() {
   return (
-    <KeyboardLayerProvider>
-      <DialogProvider>
-        <ToastProvider>
-          <box
-            alignItems="center"
-            justifyContent="center"
-            backgroundColor="#0D0D12"
-            width="100%"
-            height="100%"
-            gap={2}
-          >
-            <Header />
-            <box width="100%" maxWidth={78} paddingX={2}>
-              <Input onSubmit={() => {}} />
-            </box>
-          </box>
-        </ToastProvider>
-      </DialogProvider>
-    </KeyboardLayerProvider>
+    <ThemeProvider>
+      <KeyboardLayerProvider>
+        <DialogProvider>
+          <ToastProvider>
+            <ThemedRoot />
+          </ToastProvider>
+        </DialogProvider>
+      </KeyboardLayerProvider>
+    </ThemeProvider>
   );
 }
 
